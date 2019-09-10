901155df66f640dca79130b84c8c8854ae35914a
bfredrickson <bfredrickson@healthequity.com>
2019-09-10 15:49:47 -0600

    Db and JsonFile beginning interfaces.

--- MsfTeamBuilderAPI/Services/DataBase/IDbInterface.cs
+++ MsfTeamBuilderAPI/Services/DataBase/IDbInterface.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MsfTeamBuilderAPI.Models.Entities;
 
 namespace MsfTeamBuilderAPI.Services.DataBase
 {
-  public interface IDbInterface
+  /// <summary>
+  /// TODO: Basically Need one of these for each object. Team, Toon, etc..
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public interface IDbInterface<T> where T: BaseEntity
   {
-
+    IEnumerable<T> List { get; }
+    void Add(T entity);
+    void Delete(T entity);
+    void Update(T entity);
+    T FindById(int id);
   }
 }
--- MsfTeamBuilderAPI/Services/DataBase/JsonFileDb.cs
+++ MsfTeamBuilderAPI/Services/DataBase/JsonFileDb.cs
@@ -7,11 +7,5 @@ namespace MsfTeamBuilderAPI.Services.DataBase
 {
   public class JsonFileDb
   {
-    private readonly string _location;
-
-    public JsonFileDb(string location)
-    {
-      _location = location;
-    }
   }
 }
